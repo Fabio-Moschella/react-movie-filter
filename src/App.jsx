b6a6fdbd432455f2cbc791fb3e6563de652ee08a
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-27 16:45:56 +0200

    add-slectedgenre-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
 import { useState } from "react";
 
-function App() {}
+function App() {
+  const moviesList = [
+    { title: "Inception", genre: "Fantascienza" },
+    { title: "Il Padrino", genre: "Thriller" },
+    { title: "Titanic", genre: "Romantico" },
+    { title: "Batman", genre: "Azione" },
+    { title: "Interstellar", genre: "Fantascienza" },
+    { title: "Pulp Fiction", genre: "Thriller" },
+  ];
+  const [movies, setMovies] = useState(moviesList);
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  return (
+    <div className="container">
+      <select
+        value={selectedGenre}
+        className="form-select"
+        aria-label="Default select example"
+        onChange={(e) => setSelectedGenre(e.target.value)}
+      >
+        <option value={""}>Open menu movies</option>
+
+        {movies.map((movie, index) => (
+          <option value={movie.genre} key={index}>
+            {movie.genre}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
 
 export default App;
